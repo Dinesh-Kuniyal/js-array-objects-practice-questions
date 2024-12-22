40478e9a1652020fc958028da7a3a7c6ed2c7e5f
dinesh_kuniyal <76137603+Dinesh-Kuniyal@users.noreply.github.com>
2024-12-22 10:14:30 +0530

    Solved long strings problem --filter

--- filter_problems/set_01.js
+++ filter_problems/set_01.js
@@ -1,3 +1,20 @@
+
+// words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
+const isStringLong = function (string) {
+  return string.length > 5;
+};
+
+const filterLongWords = function (words) {
+  return words.filter(isStringLong);
+};
+
+console.log(filterLongWords([]));
+console.log(filterLongWords(["apple", "banana", "kiwi", "grape"]));
+console.log(filterLongWords(["apple", "kiwi", "grape"]));
+console.log(filterLongWords(["banana", "pineapple"]));
+
+console.log = function () { };
+
 // even numbers [1, 2, 3, 4, 5] => [2, 4]
 const isEven = function (number) {
   return number % 2 === 0;
@@ -11,5 +28,3 @@ console.log(filterEvenNumbers([]));
 console.log(filterEvenNumbers([1, 2, 3, 4]));
 console.log(filterEvenNumbers([1, 3, 5, 7]));
 console.log(filterEvenNumbers([2, 4, 6, 8]));
-
-console.log = function () { };
