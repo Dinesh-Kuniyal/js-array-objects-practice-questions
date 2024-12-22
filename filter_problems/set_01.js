4ded94c084e92edf53b5e029a9e75401c46abd30
dinesh_kuniyal <76137603+Dinesh-Kuniyal@users.noreply.github.com>
2024-12-22 10:18:55 +0530

    Solved filter adults problem --filter

--- filter_problems/set_01.js
+++ filter_problems/set_01.js
@@ -1,3 +1,18 @@
+// people older than 30 [{name: "Alice", age: 25}, {name: "Bob", age: 35}] => [{name: "Bob", age: 35}]
+const isPersonAdult = function (person) {
+  return person.age > 30;
+};
+
+const filterAdults = function (people) {
+  return people.filter(isPersonAdult);
+};
+
+console.log(filterAdults([]));
+console.log(filterAdults([{ name: "Alice", age: 25 }, { name: "Bob", age: 35 }]));
+console.log(filterAdults([{ name: "Alice", age: 25 }, { name: "Bob", age: 20 }]));
+console.log(filterAdults([{ name: "Alice", age: 35 }, { name: "Bob", age: 45 }]));
+
+console.log = function () { };
 
 // words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
 const isStringLong = function (string) {
@@ -12,8 +27,6 @@ console.log(filterLongWords([]));
 console.log(filterLongWords(["apple", "banana", "kiwi", "grape"]));
 console.log(filterLongWords(["apple", "kiwi", "grape"]));
 console.log(filterLongWords(["banana", "pineapple"]));
-
-console.log = function () { };
 
 // even numbers [1, 2, 3, 4, 5] => [2, 4]
 const isEven = function (number) {
