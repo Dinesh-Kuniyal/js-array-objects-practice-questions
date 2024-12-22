622dc87a989dd9d281a13aeca641452190bba17c
dinesh_kuniyal <76137603+Dinesh-Kuniyal@users.noreply.github.com>
2024-12-22 20:31:05 +0530

    Solved Group by date --reduce

--- reduce_problems/set_01.js
+++ reduce_problems/set_01.js
@@ -1,3 +1,35 @@
+// groupByDate([{ date: '2024-01-01', value: 10 },
+// { date: '2024-01-01', value: 20 },
+// { date: '2024-01-02', value: 30 }])
+// => { '2024-01-01': [10, 20], '2024-01-02': [30] }
+
+const groupByDate = function (records) {
+  return records.reduce((group, { date, value }) => {
+
+    const newGroup = { ...group };
+
+    const existingValue = (date in newGroup && newGroup[date]) || [];
+
+    newGroup[date] = [...existingValue, value];
+
+    return newGroup;
+  }, {});
+};
+
+console.log(groupByDate([
+  { date: '2024-01-01', value: 10 },
+  { date: '2024-01-01', value: 20 },
+  { date: '2024-01-02', value: 30 }
+]));
+
+console.log(groupByDate([
+  { date: '2024-01-01', value: 10 },
+  { date: '2024-01-01', value: 20 },
+  { date: '2024-01-01', value: 30 }
+]));
+
+console.log = function () { };
+
 // findMinMax([1, 2, 3, 4, 5]) => { min: 1, max: 5 }
 const findMinMax = function (numbers) {
   return numbers.reduce(function ({ min, max }, number) {
@@ -13,8 +45,6 @@ console.log(findMinMax([]));
 console.log(findMinMax([5, 5, 6]));
 console.log(findMinMax([5, 5, 5]));
 console.log(findMinMax([1, 2, 3, 4, 5]));
-
-console.log = function () { };
 
 // sumByCategory([{ category: 'A', value: 10 }, 
 // { category: 'B', value: 20 }, { category: 'A', value: 30 }]) 
