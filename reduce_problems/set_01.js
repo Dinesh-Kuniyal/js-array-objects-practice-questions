03b98c4a879c506d31a458b1b03c940b1a7d9e6b
dinesh_kuniyal <76137603+Dinesh-Kuniyal@users.noreply.github.com>
2024-12-22 19:30:37 +0530

    Added reduce problems set | solved sumby category --reduce

---
+++ reduce_problems/set_01.js
@@ -0,0 +1,28 @@
+// sumByCategory([{ category: 'A', value: 10 }, 
+// { category: 'B', value: 20 }, { category: 'A', value: 30 }]) 
+// => { A: 40, B: 20 }
+
+const sumByCategory = function (items) {
+  const defaultValue = {};
+
+  return items.reduce(function (categoriesSum, item) {
+
+    const newSum = { ...categoriesSum };
+    const existingSum = (item.category in newSum && newSum[item.category]) || 0;
+    newSum[item.category] = item.value + existingSum;
+
+    return newSum;
+  }, defaultValue);
+};
+
+console.log(sumByCategory([
+  { category: 'A', value: 10 },
+  { category: 'B', value: 20 },
+  { category: 'A', value: 30 }
+]));
+
+console.log(sumByCategory([
+  { category: 'A', value: 10 },
+  { category: 'B', value: 20 },
+  { category: 'C', value: 40 }
+]));
