753c5b78ef697a1d4804195a6335ee3ed522a82b
dinesh_kuniyal <76137603+Dinesh-Kuniyal@users.noreply.github.com>
2024-12-22 20:01:04 +0530

    Solved finding maximum and minimum

--- reduce_problems/set_01.js
+++ reduce_problems/set_01.js
@@ -1,10 +1,26 @@
+// findMinMax([1, 2, 3, 4, 5]) => { min: 1, max: 5 }
+const findMinMax = function (numbers) {
+  return numbers.reduce(function ({ min, max }, number) {
+    return {
+      min: Math.min(number, min),
+      max: Math.max(number, max)
+    };
+
+  }, { min: +Infinity, max: -Infinity });
+};
+
+console.log(findMinMax([]));
+console.log(findMinMax([5, 5, 6]));
+console.log(findMinMax([5, 5, 5]));
+console.log(findMinMax([1, 2, 3, 4, 5]));
+
+console.log = function () { };
+
 // sumByCategory([{ category: 'A', value: 10 }, 
 // { category: 'B', value: 20 }, { category: 'A', value: 30 }]) 
 // => { A: 40, B: 20 }
 
 const sumByCategory = function (items) {
-  const defaultValue = {};
-
   return items.reduce(function (categoriesSum, item) {
 
     const newSum = { ...categoriesSum };
@@ -12,7 +28,7 @@ const sumByCategory = function (items) {
     newSum[item.category] = item.value + categorySum;
 
     return newSum;
-  }, defaultValue);
+  }, {});
 };
 
 console.log(sumByCategory([
